aa6d716698156342471798573f311632d42fadc2
naelalaji <na2elalaje@hotmail.com>
2024-03-08 16:18:00 +0100

    supprission ressource admin

--- src/Controller/RessourcesController.php
+++ src/Controller/RessourcesController.php
@@ -6,7 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Ressource;
-
+use Doctrine\ORM\EntityManagerInterface; 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class RessourcesController extends AbstractController
 {
@@ -20,5 +21,20 @@ class RessourcesController extends AbstractController
             'ressources' => $ressources]);
     }
 
-   
+    #[Route('/ressources/supprimer/{id}', name: 'app_supprimer_ressource', methods: ['POST'])]
+    public function supprimerRessource($id, RessourcesRepository $repository, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        $ressource = $repository->find($id);
+
+        if (!$ressource) {
+            $errorMessage = 'La ressource avec l\'identifiant ' . $id . ' n\'existe pas.';
+            $this->addFlash('error', $errorMessage);
+        } else {
+            $entityManager->remove($ressource);
+            $entityManager->flush();
+            $this->addFlash('success', 'La ressource a été supprimée avec succès.');
+        }
+
+        return $this->redirectToRoute('app_ressources');
+    }
 }
