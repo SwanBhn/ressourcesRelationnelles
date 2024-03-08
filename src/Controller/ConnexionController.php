d60173b7a52f24492ae21358b6634ccaa7ab2446
mickael <mickael.phung@newdealstore.com>
2024-03-08 11:29:22 +0100

    gererConnexion

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -7,25 +7,24 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class InscriptionController extends AbstractController
+class ConnexionController extends AbstractController
 {
-    #[Route('/inscription', name: 'inscription')]
-    public function setInscription(Request $request): Response
+    #[Route('/connexion', name: 'connexion')]
+    public function setConnexion(Request $request): Response
     {
         // Récupérer les données du formulaire
-        $nomUtilisateur = $request->request->get('nomUtilisateur');
         $email = $request->request->get('email');
         $motDePasse = $request->request->get('motDePasse');
 
         // Vérifier que tous les champs sont remplis
-        if (empty($nomUtilisateur) || empty($email) || empty($motDePasse)) {
+        if (empty($email) || empty($motDePasse)) {
             $this->addFlash('error', 'Veuillez remplir tous les champs du formulaire.');
             return $this->render('gererConnexion/gererConnexion.twig', [
-                'controller_name' => 'InscriptionController',
+                'controller_name' => 'ConnexionController',
             ]);
         }
 
-        $this->addFlash('success', 'Inscription réussie ! Bienvenue, ' . $nomUtilisateur . ' !');
+        $this->addFlash('success', 'Connexion réussie !');
 
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
