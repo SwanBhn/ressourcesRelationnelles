17b584f09e5950366c5b78344f8ac929d2ed5717
naelalaji <na2elalaje@hotmail.com>
2024-06-11 10:38:06 +0200

    Modifications ressources - profile - front-end

--- assets/app.js
+++ assets/app.js
@@ -23,3 +23,58 @@ if (menuToggle) {
         }
     });
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const saveButton = document.getElementById('save-button');
+
+    let initialName = nameInput.value;
+    let initialEmail = emailInput.value;
+
+    function checkChanges() {
+        console.log("ici")
+        if (nameInput.value !== initialName || emailInput.value !== initialEmail) {
+            saveButton.disabled = false;
+        } else {
+            saveButton.disabled = true;
+        }
+    }
+
+    nameInput.addEventListener('input', checkChanges);
+    emailInput.addEventListener('input', checkChanges);
+
+    saveButton.addEventListener('click', async () => {
+        const name = nameInput.value;
+        const email = emailInput.value;
+
+        if (!name.trim() || !email.trim()) {
+            alert("Veuillez remplir tous les champs.");
+            return;
+        }
+
+        const response = await fetch('/api/user/update', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email })
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            alert(result.message);
+            // Mise à jour des valeurs initiales après une sauvegarde réussie
+            initialName = name;
+            initialEmail = email;
+            saveButton.disabled = true;
+            window.location.reload();
+        } else {
+            alert('Erreur : ' + result.message);
+        }
+    });
+});
+
+
