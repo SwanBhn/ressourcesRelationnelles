b9a6ab6e467c957cffbdc2b782656784a5a15350
Martin <martin.frontera85@gmail.com>
2024-08-21 09:56:57 +0200

    sonar

--- src/Controller/RelationsController.php
+++ src/Controller/RelationsController.php
@@ -4,14 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Amis;
 use App\Entity\User;
+use App\Exception\UserNotFoundException;
+use App\Repository\AmisRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Repository\AmisRepository;
 
 class RelationsController extends AbstractController
 {
@@ -36,7 +35,7 @@ class RelationsController extends AbstractController
         $userId = $user->getId();
 
         if (is_null($userId)) {
-            throw new Exception(self::ERROR_MESSAGE);
+            throw new UserNotFoundException();
         }
 
         $amis = $this->amisRepository->findFriendsByUserId($userId);
@@ -53,13 +52,7 @@ class RelationsController extends AbstractController
 
     private function prepareRelations(array $amis, int $userId): array
     {
-        $relations = [];
-
-        foreach ($amis as $ami) {
-            $relations[] = $this->formatRelation($ami, $userId);
-        }
-
-        return $relations;
+        return array_map(fn($ami) => $this->formatRelation($ami, $userId), $amis);
     }
 
     private function formatRelation(Amis $ami, int $userId): array
@@ -87,7 +80,7 @@ class RelationsController extends AbstractController
         $userId = $user->getId();
 
         if (is_null($userId)) {
-            throw new Exception(self::ERROR_MESSAGE);
+            throw new UserNotFoundException();
         }
 
         $deleteAmis = $entityManager->getRepository(Amis::class)
@@ -116,18 +109,20 @@ class RelationsController extends AbstractController
         $userId = $user->getId();
 
         if (is_null($userId)) {
-            throw new Exception(self::ERROR_MESSAGE);
+            throw new UserNotFoundException();
         }
 
-        $currentUser = $entityManager->getRepository(User::class)->findOneBy(['id' => $userId]);
+        $currentUser = $entityManager->getRepository(User::class)->find($userId);
+        $newFriend = $entityManager->getRepository(User::class)->find($idAmis);
 
-        $newFriend = $entityManager->getRepository(User::class)->findOneBy(['id' => $idAmis]);
-        $addAmis = new Amis();
-        $addAmis->setIdUtilisateur($currentUser);
-        $addAmis->setIdUtilisateurAmi($newFriend);
+        if ($currentUser && $newFriend) {
+            $addAmis = new Amis();
+            $addAmis->setIdUtilisateur($currentUser);
+            $addAmis->setIdUtilisateurAmi($newFriend);
 
-        $entityManager->persist($addAmis);
-        $entityManager->flush();
+            $entityManager->persist($addAmis);
+            $entityManager->flush();
+        }
 
         return $this->redirectToRoute('app_getRelations');
     }
@@ -137,74 +132,84 @@ class RelationsController extends AbstractController
     #[Route('/api/relations/{userId}', name: 'app_api_getRelations', methods: ['GET'])]
     public function getRelationsApi(EntityManagerInterface $entityManager, int $userId): Response
     {
+        // Default response and status
+        $response = new JsonResponse([], Response::HTTP_OK);
+        $status = Response::HTTP_OK;
+        $message = '';
+    
+        // Check for invalid user ID
         if (is_null($userId)) {
-            return new JsonResponse(['message' => self::ERROR_MESSAGE], Response::HTTP_NOT_FOUND);
+            $status = Response::HTTP_NOT_FOUND;
+            $message = self::ERROR_MESSAGE;
+        } else {
+            $currentUser = $entityManager->getRepository(User::class)->find($userId);
+    
+            if (is_null($currentUser)) {
+                $status = Response::HTTP_NOT_FOUND;
+                $message = self::ERROR_MESSAGE;
+            } else {
+                $amis = $this->amisRepository->findFriendsByUserId($userId);
+    
+                if (is_null($amis) || count($amis) === 0) {
+                    $status = Response::HTTP_NOT_FOUND;
+                    $message = 'Ajoutez des personnes à vos relations!';
+                } else {
+                    $tableau = array_map(function($ami) use ($userId) {
+                        $isCurrentUserFriend = $ami->getIdUtilisateurAmi()->getId() == $userId;
+                        
+                        return [
+                            'idUtilisateur' => $isCurrentUserFriend ? $ami->getIdUtilisateurAmi()->getId() : $ami->getIdUtilisateur()->getId(),
+                            'photo' => $isCurrentUserFriend ? $ami->getIdUtilisateur()->getPhoto() : $ami->getIdUtilisateurAmi()->getPhoto(),
+                            'idAmi' => $isCurrentUserFriend ? $ami->getIdUtilisateur()->getId() : $ami->getIdUtilisateurAmi()->getId(),
+                            'nom' => $isCurrentUserFriend ? $ami->getIdUtilisateur()->getNom() : $ami->getIdUtilisateurAmi()->getNom(),
+                            'description' => $isCurrentUserFriend ? $ami->getIdUtilisateur()->getDescription() : $ami->getIdUtilisateurAmi()->getDescription(),
+                        ];
+                    }, $amis);
+    
+                    $response = new JsonResponse($tableau, $status, ['Access-Control-Allow-Origin' => '*']);
+                }
+            }
         }
     
-        $currentUser = $entityManager->getRepository(User::class)->findOneBy(['id' => $userId]);
-    
-        if (is_null($currentUser)) {
-            return new JsonResponse(['message' => self::ERROR_MESSAGE], Response::HTTP_NOT_FOUND);
-        }
-    
-        $amis = $this->amisRepository->findFriendsByUserId($userId);
-    
-        if (is_null($amis)) {
-            return new JsonResponse(['message' => 'Ajoutez des personnes à vos relations!'], Response::HTTP_NOT_FOUND);
-        }
-    
-        $tableau = array_map(function($ami) use ($userId) {
-            $isCurrentUserFriend = $ami->getIdUtilisateurAmi()->getId() == $userId;
-            
-            return [
-                'idUtilisateur' => $isCurrentUserFriend ? $ami->getIdUtilisateurAmi()->getId() : $ami->getIdUtilisateur()->getId(),
-                'photo' => $isCurrentUserFriend ? $ami->getIdUtilisateur()->getPhoto() : $ami->getIdUtilisateurAmi()->getPhoto(),
-                'idAmi' => $isCurrentUserFriend ? $ami->getIdUtilisateur()->getId() : $ami->getIdUtilisateurAmi()->getId(),
-                'nom' => $isCurrentUserFriend ? $ami->getIdUtilisateur()->getNom() : $ami->getIdUtilisateurAmi()->getNom(),
-                'description' => $isCurrentUserFriend ? $ami->getIdUtilisateur()->getDescription() : $ami->getIdUtilisateurAmi()->getDescription(),
-            ];
-        }, $amis);
-    
-        return new JsonResponse($tableau, 200, ['Access-Control-Allow-Origin' => '*']);
+        return new JsonResponse(['message' => $message], $status, ['Access-Control-Allow-Origin' => '*']);
     }
     
-
 
     #[Route('/api/relations/{userId}/ami/{idAmi}', name: 'app_api_deleteRelations', methods: ['DELETE'])]
     public function deleteRelationsApi(EntityManagerInterface $entityManager, int $userId, int $idAmi): Response
     {
-        $response = new Response('', Response::HTTP_NOT_FOUND);
-
-        if (!is_null($userId)) {
-            $currentUser = $entityManager->getRepository(User::class)->findOneBy(['id' => $userId]);
-
-            if ($currentUser) {
-                $amis = $this->amisRepository->findFriendsByUserId($userId);
-
-                if ($amis) {
-                    $deleteAmis = $entityManager->getRepository(Amis::class)
-                        ->findOneBy([
-                            'idUtilisateur' => $userId,
-                            'idUtilisateurAmi' => $idAmi
-                        ]);
-
-                    if (!$deleteAmis) {
-                        $deleteAmis = $entityManager->getRepository(Amis::class)
-                            ->findOneBy([
-                                'idUtilisateur' => $idAmi,
-                                'idUtilisateurAmi' => $userId
-                            ]);
-                    }
-
-                    if ($deleteAmis) {
-                        $entityManager->remove($deleteAmis);
-                        $entityManager->flush();
-                        $response = new Response('', Response::HTTP_OK);
-                    }
-                }
-            }
+        if (is_null($userId)) {
+            return new Response('', Response::HTTP_NOT_FOUND);
         }
 
-        return $response;
+        $currentUser = $entityManager->getRepository(User::class)->find($userId);
+
+        if (!$currentUser) {
+            return new Response('', Response::HTTP_NOT_FOUND);
+        }
+
+        $amis = $this->amisRepository->findFriendsByUserId($userId);
+
+        if (!$amis) {
+            return new Response('', Response::HTTP_NOT_FOUND);
+        }
+
+        $deleteAmis = $entityManager->getRepository(Amis::class)
+            ->findOneBy([
+                'idUtilisateur' => $userId,
+                'idUtilisateurAmi' => $idAmi
+            ]) ?? $entityManager->getRepository(Amis::class)
+                ->findOneBy([
+                    'idUtilisateur' => $idAmi,
+                    'idUtilisateurAmi' => $userId
+                ]);
+
+        if ($deleteAmis) {
+            $entityManager->remove($deleteAmis);
+            $entityManager->flush();
+            return new Response('', Response::HTTP_OK);
+        }
+
+        return new Response('', Response::HTTP_NOT_FOUND);
     }
 }
